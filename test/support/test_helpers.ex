9daf9103f5a4af884a50ce5d60f2c7147cef76a3
Bryon Winger <bryonwinger@gmail.com>
2017-07-23 11:23:44 -0500

    forgot newer files

---
+++ test/support/test_helpers.ex
@@ -0,0 +1,29 @@
+defmodule Rumbl.TestHelpers do
+  alias Rumbl.Repo
+
+  def insert_user(attrs \\ %{}) do
+    changes = Dict.merge(%{
+      name: "Joe User",
+      username: "user#{Base.encode16(:crypto.strong_rand_bytes(8))}",
+      password: "supersecret"
+      }, attrs)
+
+    %Rumbl.User{}
+    |> Rumbl.User.registration_changeset(changes)
+    |> Repo.insert!()
+  end
+
+  def insert_video(user, attrs \\ %{}) do
+    user
+    |> Ecto.build_assoc(:videos, attrs)
+    |> Repo.insert!()
+  end
+
+  def insert_category(attrs \\ %{}) do
+    changes = Dict.merge(%{name: "myname"}, attrs)
+
+    %Rumbl.Category{}
+    |> Rumbl.Category.changeset(changes)
+    |> Repo.insert!()
+  end
+end
