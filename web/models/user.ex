48f9151aaeba033e0351195f893d490e99582747
Bryon Winger <bryonwinger@gmail.com>
2017-07-30 11:46:08 -0500

    passing user tests

--- web/models/user.ex
+++ web/models/user.ex
@@ -14,6 +14,7 @@ defmodule Rumbl.User do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, ~w(name username))
+    |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
     |> unique_constraint(:username)
   end
@@ -22,6 +23,7 @@ defmodule Rumbl.User do
     model
     |> changeset(params)
     |> cast(params, ~w(password))
+    |> validate_required([:password])
     |> validate_length(:password, min: 6, max: 100)
     |> put_pass_hash()
   end
